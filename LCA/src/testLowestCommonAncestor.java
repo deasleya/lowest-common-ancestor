4651ae2adb229dc30474c7272b5a9fab405bde07
Aine Deasley <deasleya@itserv.scss.tcd.ie>
2019-09-25 09:34:54 +0100

    Created Node object

--- LCA/src/testLowestCommonAncestor.java
+++ LCA/src/testLowestCommonAncestor.java
@@ -1,12 +1,23 @@
 import static org.junit.Assert.*;
+//import static org.junit.Assert.assertThat;
 
 import org.junit.Test;
 
 public class testLowestCommonAncestor {
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void testEmptyTree() {
+		Node root = createBinaryTree();
+		assertNotNull("The binary tree is empty", root);
+		//fail("Not yet implemented");
+	}
+	
+	@Test
+	public void testInvalidSearchParameter() {
+		lowestCommonAncestor(root, c, d);
+		Node root = createBinaryTree();
+		assertNotNull("The binary tree is empty", root);
+		//fail("Not yet implemented");
 	}
 
 }
