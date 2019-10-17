447b811b8a47e92181f773d502e62466821cf9ab
Aine Deasley <deasleya@ITSERV.SCSS.TCD.IE>
2019-10-17 15:43:59 +0100

    Changed name of binary tree LCA class to better reflect purpose

---
+++ LCA/src/BinaryTreeLCA.java
@@ -0,0 +1,140 @@
+// LCA DEVELOPMENT TASK 1 - BINARY TREE IMPLEMENTATION
+
+// largely based on geeksforgeeks.org  code 
+
+import java.util.ArrayList;
+import java.util.List; 
+
+
+// Node class
+class Node { 
+	int data; 
+	Node left, right; 
+
+	Node(int value) { 
+		data = value; 
+		left = right = null; 
+	} 
+} 
+
+public class BinaryTreeLCA 
+{
+
+	Node root; //initialise root
+	List<Integer> path1 = new ArrayList<>(); //creates list for node 1's path
+	List<Integer> path2 = new ArrayList<>(); // creates list for node 2's path
+
+	
+	private Node addRecursive(Node current, int value) {
+		if (current == null) {
+			return new Node(value);
+		}
+
+		if (value < current.data) {
+			current.left = addRecursive(current.left, value);
+		} else if (value > current.data) {
+			current.right = addRecursive(current.right, value);
+		} else {
+			// value already exists
+			return current;
+		}
+
+		return current;
+	}
+
+	public void add(int value) {
+		root = addRecursive(root, value);
+	}
+
+	private Node deleteRecursive(Node current, int value) {
+		if (current == null) {
+			return current;
+		}
+
+		if (value < current.data) {
+			current.left = deleteRecursive(current.left, value);
+		}
+		else if (value > current.data) {
+			current.right = deleteRecursive(current.right, value);
+		}
+		else {
+			if(current.left == null)
+				return current.right;
+			else if (current.right == null) 
+				return current.left;
+			current.data = findSmallestValue(root.right);
+			current.right = deleteRecursive(root.right, root.data); 
+		}
+		return current;
+	}
+
+	private int findSmallestValue(Node root) {
+		return root.left == null ? root.data : findSmallestValue(root.left);
+	}
+
+	public void delete(int value) {
+		root = deleteRecursive(root, value);
+		System.out.println("Node with value " + value + " has been deleted");
+	}
+
+
+
+	// Finds the path from root node to given root of the tree. 
+	int findLCA(int n1, int n2) { 
+		path1.clear(); 
+		path2.clear(); 
+		return findLCAInternal(root, n1, n2); 
+	} 
+
+	private int findLCAInternal(Node root, int n1, int n2) { 
+
+		if (!findPath(root, n1, path1) || !findPath(root, n2, path2)) { 
+			System.out.println((path1.size() > 0) ? "n1 is present" : "n1 is missing"); 
+			System.out.println((path2.size() > 0) ? "n2 is present" : "n2 is missing"); 
+			return -1; 
+		} 
+
+		int i; 
+		for (i = 0; i < path1.size() && i < path2.size(); i++) { 
+
+			// System.out.println(path1.get(i) + " " + path2.get(i)); 
+			if (!path1.get(i).equals(path2.get(i))) 
+				break; 
+		} 
+
+		return path1.get(i-1); 
+	} 
+
+	// Finds the path from root node to given root of the tree, Stores the 
+	// path in a vector path[], returns true if path exists otherwise false 
+	private boolean findPath(Node root, int n, List<Integer> path) 
+	{ 
+		// base case 
+		if (root == null) { 
+			return false; 
+		} 
+
+		// Store this node . The node will be removed if 
+		// not in path from root to n. 
+		path.add(root.data); 
+
+		if (root.data == n) { 
+			return true; 
+		} 
+
+		if (root.left != null && findPath(root.left, n, path)) { 
+			return true; 
+		} 
+
+		if (root.right != null && findPath(root.right, n, path)) { 
+			return true; 
+		} 
+
+		// If not present in subtree rooted with root, remove root from 
+		// path[] and return false 
+		path.remove(path.size()-1); 
+
+		return false; 
+	} 
+
+}
